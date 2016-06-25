1b1068033c1825282bb9c79b4e6aa40d9f67d4d5
weijarz <weijarz@gmail.com>
2016-06-25 13:29:14 +0800

    Use loose mode

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     plugins: [
+        require("babel-plugin-transform-decorators-legacy").default,
         require("babel-plugin-transform-class-properties"),
         require("babel-plugin-transform-object-rest-spread"),
         require("babel-plugin-transform-es3-member-expression-literals"),
@@ -15,11 +16,11 @@ module.exports = {
         require("babel-plugin-transform-es2015-function-name"),
         require("babel-plugin-transform-es2015-arrow-functions"),
         require("babel-plugin-transform-es2015-block-scoped-functions"),
-        require("babel-plugin-transform-es2015-classes"),
+        [require("babel-plugin-transform-es2015-classes"), {loose: true}],
         require("babel-plugin-transform-es2015-object-super"),
         require("babel-plugin-transform-es2015-shorthand-properties"),
         require("babel-plugin-transform-es2015-duplicate-keys"),
-        require("babel-plugin-transform-es2015-computed-properties"),
+        [require("babel-plugin-transform-es2015-computed-properties"), {loose: true}],
         require("babel-plugin-transform-es2015-for-of"),
         require("babel-plugin-transform-es2015-sticky-regex"),
         require("babel-plugin-transform-es2015-unicode-regex"),
